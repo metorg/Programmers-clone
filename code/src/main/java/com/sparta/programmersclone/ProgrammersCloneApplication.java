cf174cee4b240e4cce34c892c3ed9959808c18db
geonyeolpark <msmn1729@gmail.com>
2021-04-05 17:54:13 +0900

    환경 세팅

---
+++ code/src/main/java/com/sparta/programmersclone/ProgrammersCloneApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.programmersclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammersCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammersCloneApplication.class, args);
+    }
+
+}
---
+++ code/src/test/java/com/sparta/programmersclone/ProgrammersCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.programmersclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammersCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
